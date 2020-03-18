9a254d20890731f4e1d921e2848cbe59a41ea306
Fabry Levente <fabry.levente@allwin.hu>
2020-03-18 19:28:42 +0100

    create owner with dogs option

--- RenosFriendsList.API/Models/OwnerForCreationDto.cs
+++ RenosFriendsList.API/Models/OwnerForCreationDto.cs
@@ -1,9 +1,13 @@
-﻿namespace RenosFriendsList.API.Models
+﻿using System.Collections.Generic;
+
+namespace RenosFriendsList.API.Models
 {
     public class OwnerForCreationDto
     {
         public string Name { get; set; }
 
         public string Description { get; set; }
+
+        public ICollection<DogForCreationDto> Dogs { get; set; } = new List<DogForCreationDto>();
     }
 }
