7d86de3fc3ccea1b66a0c9a4b2ca1e0dd8804c78
Fabry Levente <fabry.levente@allwin.hu>
2020-03-30 08:25:35 +0200

    add get dog to dog controller + options

--- RenosFriendsList.API/Controllers/DogsController.cs
+++ RenosFriendsList.API/Controllers/DogsController.cs
@@ -28,5 +28,25 @@ namespace RenosFriendsList.API.Controllers
             var dogsFromRepo = _dogRepository.GetAllDogs(parameters);
             return Ok(_mapper.Map<IEnumerable<DogDto>>(dogsFromRepo));
         }
+
+        [HttpHead]
+        [HttpGet("{dogId}")]
+        public ActionResult<DogDto> GetDog(int dogId)
+        {
+            var dogFromRepo = _dogRepository.GetDog(dogId);
+            if (dogFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<DogDto>(dogFromRepo));
+        }
+
+        [HttpOptions]
+        public IActionResult GetDogsOptions()
+        {
+            Response.Headers.Add("Allow", "GET,OPTIONS,HEAD");
+            return Ok();
+        }
     }
 }
